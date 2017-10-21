f6fdf19a9769c449515c900794a64f73bde4d83b
mohamed essam <mohamed.essam9393@gmail.com>
2017-10-21 15:36:08 +0200

    m

--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -65,4 +65,37 @@ export class UserProvider {
     })
     return promise;
 }
+//edit account
+getuserdetails() {
+  var promise = new Promise((resolve, reject) => {
+  this.firedata.child(firebase.auth().currentUser.uid).once('value', (snapshot) => {
+    resolve(snapshot.val());
+  }).catch((err) => {
+    reject(err);
+    })
+  })
+  return promise;
+}
+//edit name
+updatedisplayname(newname) {
+  var promise = new Promise((resolve, reject) => {
+    this.afireauth.auth.currentUser.updateProfile({
+    displayName: newname,
+    photoURL: this.afireauth.auth.currentUser.photoURL
+  }).then(() => {
+    this.firedata.child(firebase.auth().currentUser.uid).update({
+      displayName: newname,
+      photoURL: this.afireauth.auth.currentUser.photoURL,
+      uid: this.afireauth.auth.currentUser.uid
+    }).then(() => {
+      resolve({ success: true });
+    }).catch((err) => {
+      reject(err);
+    })
+    }).catch((err) => {
+      reject(err);
+  })
+  })
+  return promise;
+}
 }
